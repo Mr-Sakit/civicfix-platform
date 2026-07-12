9089329f0e93e20a4ac4c79e2d54ac403d5f9433
Mr-Sakit <sakitazeyek@gmail.com>
2026-07-12 12:48:59 +0400

    Handle Azure PostgreSQL zone drift

--- infrastructure/terraform/azure/main.tf
+++ infrastructure/terraform/azure/main.tf
@@ -156,6 +156,12 @@ resource "azurerm_postgresql_flexible_server" "main" {
   depends_on = [
     azurerm_private_dns_zone_virtual_network_link.postgres
   ]
+
+  lifecycle {
+    ignore_changes = [
+      zone
+    ]
+  }
 }
 
 resource "azurerm_postgresql_flexible_server_database" "app" {
